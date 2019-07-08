5d0cc624e0c64acdbd11cf5f380dc54b75542e79
LSAfrika <lstudiosafrika@gmail.com>
2019-07-08 17:04:24 +0300

    added budges tag and included ads button

--- src/app/services/global-service.service.ts
+++ src/app/services/global-service.service.ts
@@ -26,7 +26,7 @@ export class GlobalServiceService {
    loadAppRecipies() {
     this.AppRecipies.push(
       {
-        title: 'Chocolate Cake',
+        title: 'Vanilla Cake',
       imageUrl: '/assets/cakes/chocolate.jpg',
       ingredients: [{ingredient: '1 cup white sugar'},
                     {ingredient: '1/2 cup butter'},
@@ -44,7 +44,76 @@ export class GlobalServiceService {
        {procedure: 'Finally stir in the milk until batter is smooth.'},
        {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
        {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
-       {procedure:  ' Cake is done when it springs back to the touch}'}
+       {procedure:  ' Cake is done when it springs back to the touch'}
+       ] ,
+
+      },
+      {
+        title: 'banana Cake',
+      imageUrl: '/assets/cakes/chocolate.jpg',
+      ingredients: [{ingredient: '1 cup white sugar'},
+                     {ingredient: '1/2 cup butter'},
+                      {ingredient: '2 eggs '},
+                   { ingredient: ' 2 teaspoons vanilla extract'},
+                     {ingredient: ' 1 1/2 cups all-purpose flour' },
+                     {ingredient: ' 1 3/4 teaspoons baking powder '},
+                     {ingredient: ' 1/2 cup milk'}
+                    ],
+      procedure: [
+        {procedure: ' Preheat oven to 350 degrees F (175 degrees C)'},
+      {procedure: 'Grease and flour a 9x9 inch pan or line a muffin pan with paper liners In a medium bowl'},
+       {procedure: ' cream together the sugar and butter Beat in the eggs one at a time then stir in the vanilla'},
+       {procedure: 'Combine flour and baking powder, add to the creamed mixture and mix well.'},
+       {procedure: 'Finally stir in the milk until batter is smooth.'},
+       {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
+       {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
+       {procedure:  ' Cake is done when it springs back to the touch'}
+       ] ,
+
+      },
+      {
+        title: 'apple Cake',
+      imageUrl: '/assets/cakes/chocolate.jpg',
+      ingredients: [{ingredient: '1 cup white sugar'},
+                     {ingredient: '1/2 cup butter'},
+                      {ingredient: '2 eggs '},
+                   { ingredient: ' 2 teaspoons vanilla extract'},
+                     {ingredient: ' 1 1/2 cups all-purpose flour' },
+                     {ingredient: ' 1 3/4 teaspoons baking powder '},
+                     {ingredient: ' 1/2 cup milk'}
+                    ],
+      procedure: [
+        {procedure: ' Preheat oven to 350 degrees F (175 degrees C)'},
+      {procedure: 'Grease and flour a 9x9 inch pan or line a muffin pan with paper liners In a medium bowl'},
+       {procedure: ' cream together the sugar and butter Beat in the eggs one at a time then stir in the vanilla'},
+       {procedure: 'Combine flour and baking powder, add to the creamed mixture and mix well.'},
+       {procedure: 'Finally stir in the milk until batter is smooth.'},
+       {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
+       {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
+       {procedure:  ' Cake is done when it springs back to the touch'}
+       ] ,
+
+      },
+      {
+        title: 'vanilla icecream chocolate Cake',
+      imageUrl: '/assets/cakes/chocolate.jpg',
+      ingredients: [{ingredient: '1 cup white sugar'},
+                     {ingredient: '1/2 cup butter'},
+                      {ingredient: '2 eggs '},
+                   { ingredient: ' 2 teaspoons vanilla extract'},
+                     {ingredient: ' 1 1/2 cups all-purpose flour' },
+                     {ingredient: ' 1 3/4 teaspoons baking powder '},
+                     {ingredient: ' 1/2 cup milk'}
+                    ],
+      procedure: [
+        {procedure: ' Preheat oven to 350 degrees F (175 degrees C)'},
+      {procedure: 'Grease and flour a 9x9 inch pan or line a muffin pan with paper liners In a medium bowl'},
+       {procedure: ' cream together the sugar and butter Beat in the eggs one at a time then stir in the vanilla'},
+       {procedure: 'Combine flour and baking powder, add to the creamed mixture and mix well.'},
+       {procedure: 'Finally stir in the milk until batter is smooth.'},
+       {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
+       {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
+       {procedure:  ' Cake is done when it springs back to the touch'}
        ] ,
 
       },
@@ -67,7 +136,7 @@ export class GlobalServiceService {
        {procedure: 'Finally stir in the milk until batter is smooth.'},
        {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
        {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
-       {procedure:  ' Cake is done when it springs back to the touch}'}
+       {procedure:  ' Cake is done when it springs back to the touch'}
        ] ,
 
       },
@@ -90,76 +159,7 @@ export class GlobalServiceService {
        {procedure: 'Finally stir in the milk until batter is smooth.'},
        {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
        {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
-       {procedure:  ' Cake is done when it springs back to the touch}'}
-       ] ,
-
-      },
-      {
-        title: 'Chocolate Cake',
-      imageUrl: '/assets/cakes/chocolate.jpg',
-      ingredients: [{ingredient: '1 cup white sugar'},
-                     {ingredient: '1/2 cup butter'},
-                      {ingredient: '2 eggs '},
-                   { ingredient: ' 2 teaspoons vanilla extract'},
-                     {ingredient: ' 1 1/2 cups all-purpose flour' },
-                     {ingredient: ' 1 3/4 teaspoons baking powder '},
-                     {ingredient: ' 1/2 cup milk'}
-                    ],
-      procedure: [
-        {procedure: ' Preheat oven to 350 degrees F (175 degrees C)'},
-      {procedure: 'Grease and flour a 9x9 inch pan or line a muffin pan with paper liners In a medium bowl'},
-       {procedure: ' cream together the sugar and butter Beat in the eggs one at a time then stir in the vanilla'},
-       {procedure: 'Combine flour and baking powder, add to the creamed mixture and mix well.'},
-       {procedure: 'Finally stir in the milk until batter is smooth.'},
-       {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
-       {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
-       {procedure:  ' Cake is done when it springs back to the touch}'}
-       ] ,
-
-      },
-      {
-        title: 'Chocolate Cake',
-      imageUrl: '/assets/cakes/chocolate.jpg',
-      ingredients: [{ingredient: '1 cup white sugar'},
-                     {ingredient: '1/2 cup butter'},
-                      {ingredient: '2 eggs '},
-                   { ingredient: ' 2 teaspoons vanilla extract'},
-                     {ingredient: ' 1 1/2 cups all-purpose flour' },
-                     {ingredient: ' 1 3/4 teaspoons baking powder '},
-                     {ingredient: ' 1/2 cup milk'}
-                    ],
-      procedure: [
-        {procedure: ' Preheat oven to 350 degrees F (175 degrees C)'},
-      {procedure: 'Grease and flour a 9x9 inch pan or line a muffin pan with paper liners In a medium bowl'},
-       {procedure: ' cream together the sugar and butter Beat in the eggs one at a time then stir in the vanilla'},
-       {procedure: 'Combine flour and baking powder, add to the creamed mixture and mix well.'},
-       {procedure: 'Finally stir in the milk until batter is smooth.'},
-       {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
-       {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
-       {procedure:  ' Cake is done when it springs back to the touch}'}
-       ] ,
-
-      },
-      {
-        title: 'Chocolate Cake',
-      imageUrl: '/assets/cakes/chocolate.jpg',
-      ingredients: [{ingredient: '1 cup white sugar'},
-                     {ingredient: '1/2 cup butter'},
-                      {ingredient: '2 eggs '},
-                   { ingredient: ' 2 teaspoons vanilla extract'},
-                     {ingredient: ' 1 1/2 cups all-purpose flour' },
-                     {ingredient: ' 1 3/4 teaspoons baking powder '},
-                     {ingredient: ' 1/2 cup milk'}
-                    ],
-      procedure: [
-        {procedure: ' Preheat oven to 350 degrees F (175 degrees C)'},
-      {procedure: 'Grease and flour a 9x9 inch pan or line a muffin pan with paper liners In a medium bowl'},
-       {procedure: ' cream together the sugar and butter Beat in the eggs one at a time then stir in the vanilla'},
-       {procedure: 'Combine flour and baking powder, add to the creamed mixture and mix well.'},
-       {procedure: 'Finally stir in the milk until batter is smooth.'},
-       {procedure: ' Pour or spoon batter into the prepared pan Bake for 30 to 40 minutes in the preheated oven.'},
-       {procedure:  'For cupcakes, bake 20 to 25 minutes.'},
-       {procedure:  ' Cake is done when it springs back to the touch}'}
+       {procedure:  ' Cake is done when it springs back to the touch'}
        ] ,
 
       }

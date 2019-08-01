9f0d44d5c5d2ddbc428583312c4e389907cea95f
LSAfrika <lstudiosafrika@gmail.com>
2019-08-01 15:57:02 +0300

    redid recipe view page and TS logic

--- src/app/view-recepie-modal/view-recepie-modal.page.ts
+++ src/app/view-recepie-modal/view-recepie-modal.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GlobalServiceService } from '../services/global-service.service';
 import { RecepieInterface } from './../interfaces/recepie-interface';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-view-recepie-modal',
@@ -14,7 +15,7 @@ export class ViewRecepieModalPage implements OnInit {
   RecipeView: RecepieInterface;
   IsPersonalRecipe = false;
 
-  constructor( public GService: GlobalServiceService, private router: Router) { }
+  constructor( public GService: GlobalServiceService, private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
     this.RecipeView = this.GService.ViewRecipie;
@@ -33,7 +34,37 @@ export class ViewRecepieModalPage implements OnInit {
   EditPersonalRecepie( isEditable: boolean) {
     this.GService.isRecipeEditable = isEditable;
   //  this.GService.loadSpecificPersonalRecipe(title);
-   
+
   }
+
+  DeleteRecipe() {
+
+    this.DeleteNotifier();
+  }
+
+  async DeleteNotifier() {
+    const alert = await this.alertController.create({
+      header: `DELETE RECIPE`,
+      message: `are you sure you want to delete <strong> ${this.RecipeView.title}</strong>`,
+      backdropDismiss: false,
+      buttons: [
+
+        {
+          text: 'cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            this.GService.DeletePersonalRecepies();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
 
 }

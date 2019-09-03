cfb3ddeefccf9c9f44610df0718eb467380ffeb7
LSAfrika <lstudiosafrika@gmail.com>
2019-09-03 15:10:21 +0300

    added functionality to the buttons

--- src/app/PopOverfolder/popover.component.ts
+++ src/app/PopOverfolder/popover.component.ts
@@ -1,5 +1,6 @@
+import { OrderManagerService } from './../services/orderManager.service';
 import { Component, OnInit } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
+import { PopoverController, AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,13 +10,52 @@ import { Router } from '@angular/router';
 })
 export class PopoverComponent implements OnInit {
 
-  constructor(private popover: PopoverController, private router: Router ) { }
+  constructor(private popover: PopoverController,
+              private router: Router,
+              public Ordermanager: OrderManagerService,
+              private alertController: AlertController ) { }
 
   ngOnInit() {}
 
   dissmiss() {
     this.router.navigate(['/tabs/tab5']);
     this.popover.dismiss();
+  }
+
+  editOrder(iseditable?: boolean) {
+    this.Ordermanager.isEditable = iseditable;
+    console.log('iseditable value: ', iseditable);
+    console.log('active order: ', this.Ordermanager.FilteredOrder);
+    this.router.navigate(['/orders-creation-modal']);
+    this.popover.dismiss();
+  }
+
+
+  async OrderDeleteNotifier() {
+    this.popover.dismiss();
+    const alert = await this.alertController.create({
+      header: `deleted content can't be recovered`,
+      message: `are you sure want to delete order?`,
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.Ordermanager.DeletaActiveOrder();
+            this.router.navigate(['/tabs/tab5']);
+
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+
+        }
+      ]
+    });
+
+    await alert.present();
+
   }
 
 }
